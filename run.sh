e7c57fca06f8d04a969a3c856311ac7d6cc6a840
Thomas P <60588709+th-ms@users.noreply.github.com>
2023-05-04 10:09:31 -0500

    Updated run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
-if !command -v python &> /dev/null
-then
-    echo "Python could not be found. Please install Python and try again."
+if command -v python >/dev/null 2>&1; then
+    PYTHON_CMD="python"
+elif command -v python3 >/dev/null 2>&1; then
+    PYTHON_CMD="python3"
+elif command -v py >/dev/null 2>&1; then
+    PYTHON_CMD="py"
+else
+    echo "Python is not installed or not in the PATH. Please install Python or add it to the PATH."
     exit 1
 fi
-python disasm.py $1
+
+$PYTHON_CMD disasm.py $1
 read -p "Press enter to continue"
